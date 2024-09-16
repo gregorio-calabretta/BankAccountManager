4b55b4e9496e1a867a0a1532c18c60692ceb9402
gregorio-calabretta <gregoriocalabretta94@gmail.com>
2024-09-16 12:24:08 +0200

    first commit

---
+++ src/test/java/Aitho/BankAccountManager/BankAccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Aitho.BankAccountManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
